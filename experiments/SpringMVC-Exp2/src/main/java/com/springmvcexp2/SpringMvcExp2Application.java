4cd4d19c9dd66bcb9b0e0041382bab5c9ef03321
Tenerife <2892015852@qq.com>
2025-12-18 15:31:53 +0800

    SpringMVC-Exp2

---
+++ experiments/SpringMVC-Exp2/src/main/java/com/springmvcexp2/SpringMvcExp2Application.java
@@ -0,0 +1,13 @@
+package com.springmvcexp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcExp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcExp2Application.class, args);
+    }
+
+}
---
+++ experiments/SpringMVC-Exp2/src/test/java/com/springmvcexp2/SpringMvcExp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvcexp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcExp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
