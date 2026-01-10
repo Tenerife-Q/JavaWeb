b93b2b2fbd139e30f324d9021be56cf16814bbcf
Tenerife <2892015852@qq.com>
2026-01-10 22:23:21 +0800

    Create LogAspect.java

---
+++ experiments/experiment01/spring-comprehensive-exercise/src/main/java/com/lab/aspect/LogAspect.java
@@ -0,0 +1,34 @@
+package com.lab.aspect;
+
+/**
+ * @author Tenerife
+ * Su san
+ * @date 2026/1/10 22:11
+ */
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    // 考点：前置通知 + 获取参数
+    @Before("execution(* com.lab.service.UserService.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        System.out.println("====== [AOP @Before] Method: " + methodName + ", Args: " + Arrays.toString(args));
+    }
+
+    // 考点：返回后通知 + 获取返回值 (returning 属性)
+    @AfterReturning(pointcut = "execution(* com.lab.service.UserService.processUser(..))", returning = "result")
+    public void logResult(Object result) {
+        System.out.println("====== [AOP @AfterReturning] User processed. Result ID: " + result);
+    }
+}
