4863881cd7ad724be0f5febec6621660cb5115ed
Tenerife <2892015852@qq.com>
2026-01-14 19:18:03 +0800

    Create MyCryptoWalletApplicationTests.java
    MVC Thymeleaf Mybatis all

---
+++ experiments/experiment01/MyCryptoWallet/src/test/java/com/github/wallet/MyCryptoWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCryptoWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
